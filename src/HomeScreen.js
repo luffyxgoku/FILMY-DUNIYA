63f64326ddd031580a5d30f2feef6be7c0b02aa7
Shadman <khanshadman7777@gmail.com>
2023-02-25 16:31:28 +0530

    first-commit

---
+++ src/HomeScreen.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./HomeScreen.css";
+import Nav from "./Nav";
+
+function HomeScreen() {
+  return (
+    <div className="homeScreen">
+      <Nav />
+
+      {/*banner*/}
+
+      {/*row*/}
+    </div>
+  );
+}
+
+export default HomeScreen;
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCabhFBWksniRrBhDgL1T6x6Jxp33naPM8",
+  authDomain: "netflix-clone-5d5f7.firebaseapp.com",
+  projectId: "netflix-clone-5d5f7",
+  storageBucket: "netflix-clone-5d5f7.appspot.com",
+  messagingSenderId: "360775030195",
+  appId: "1:360775030195:web:0b1b0811a08347070b67c6",
+};
