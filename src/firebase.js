9b37fca03d44a158e1a91057d25fdfdae553515a
Shadman <khanshadman7777@gmail.com>
2023-03-04 15:06:23 +0530

    work done till 1:35:00

--- src/firebase.js
+++ src/firebase.js
@@ -1,8 +1,8 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyCabhFBWksniRrBhDgL1T6x6Jxp33naPM8",
-  authDomain: "netflix-clone-5d5f7.firebaseapp.com",
-  projectId: "netflix-clone-5d5f7",
-  storageBucket: "netflix-clone-5d5f7.appspot.com",
-  messagingSenderId: "360775030195",
-  appId: "1:360775030195:web:0b1b0811a08347070b67c6",
+  apiKey: "AIzaSyD0HDrUMFE8JoNcIQfHC8TEUx6ZJ_bZK8w",
+  authDomain: "netflix-clone-b56a3.firebaseapp.com",
+  projectId: "netflix-clone-b56a3",
+  storageBucket: "netflix-clone-b56a3.appspot.com",
+  messagingSenderId: "165282748048",
+  appId: "1:165282748048:web:5e607e00b9b8cea5d52a15",
 };
