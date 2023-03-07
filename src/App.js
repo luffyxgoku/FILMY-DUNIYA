dd0a45991cf0d5e94f7b0e2711f4d3316166b888
G. Raj Kumar <grajkumar2512@gmail.com>
2023-03-07 11:33:59 +0530

    added signin and signup feature with firebase

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import HomeScreen from "./screens/HomeScreen";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import LoginScreen from "./screens/LoginScreen";
+import { auth } from "./firebase";
+import { useDispatch, useSelector } from "react-redux";
+import { login, logout, selectUser } from "./features/userSlice";
 
 function App() {
-  const user = null;
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((userAuth) => {
+      if (userAuth) {
+        //logged in
+        dispatch(
+          login({
+            uid: userAuth.uid,
+            email: userAuth.email,
+          })
+        );
+      } else {
+        //logged out
+        dispatch(logout);
+      }
+    });
+    return unsubscribe;
+  }, []);
 
   return (
     <div className="app">
@@ -18,8 +40,8 @@ function App() {
               <HomeScreen />
             </Route>
           </Switch>
-        )} 
-      </Router>    
+        )}
+      </Router>
     </div>
   );
 }
--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,8 @@
+import "firebase/auth";
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
+
 const firebaseConfig = {
   apiKey: "AIzaSyD0HDrUMFE8JoNcIQfHC8TEUx6ZJ_bZK8w",
   authDomain: "netflix-clone-b56a3.firebaseapp.com",
@@ -6,3 +11,11 @@ const firebaseConfig = {
   messagingSenderId: "165282748048",
   appId: "1:165282748048:web:5e607e00b9b8cea5d52a15",
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export { auth, provider };
+export default db;
